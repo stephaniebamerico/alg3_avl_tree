4dcf5b467d9ee5380b5105aa90be22bd32c2e009
Stephanie Briere <sba16@inf.ufpr.br>
2017-05-20 19:36:18 -0300

    Comecei a insert, fiz a função que retorna a altura do maior filho e alterei o local de algumas funções para fazer sentido estruturalmente
    Signed-off-by: Stephanie Briere <sba16@inf.ufpr.br>

--- AVL_tree.c
+++ AVL_tree.c
@@ -1,16 +1,39 @@
-/*== Stephanie Briere Americo
-		GRR20165313 ==*/
-/*== Talita Halboth Cunha Fernandes
-		GRR20165399 ==*/
+/* Stephanie Briere Americo
+         GRR20165313         */
+/* Talita Halboth Cunha Fernandes
+         GRR20165399         */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "AVL_Tree"
 
+void balance (AVL_Node *node);
+AVL_Node* newNode(int key);
+AVL_Node rotateLeft (AVL_Node *node);
+AVL_Node rotateRight (AVL_Node *node);
 int getBalanceFactor (AVL_Node node);
+int maxChildrenHeight (AVL_Node node);
 
-void insertNode (AVL_Node *node, int key) {
+AVL_Node* insertNode (AVL_Node *node, int key) {
+    /* If the node does not exist, it creates 
+       and no change in its height is required
+       (sheet has height 0) */
+    if (!node)
+        return newNode(key);
+    
+    /* If the node has a key larger than the new entry, 
+       it creates the new node in the left subtree. */
+    if (node->key > key)
+        insertNode(node->left);
 
+    /* If the node has a key less than or equal 
+       to the new entry, it creates the new node
+       in the right subtree. */
+    else
+        insertNode(node->right);
+
+    // Updates node height
+    node->height = 1 + maxChildrenHeight(node);
 }
 
 void removeNode (AVL_Node *node, int key) {
@@ -21,7 +44,7 @@ void searchNode (AVL_Node *node, int key) {
     AVL_Node * aux = node;
 
     while (aux) {
-        if(aux == root)
+        if (aux == root)
           printf("%d\n", aux->key);  
         else
           printf(",%d\n", aux->key);
@@ -117,4 +140,11 @@ int getBalanceFactor (AVL_Node node) {
     l = (node.left)  ? node.left.height  : 0;
     r = (node.right) ? node.right.height : 0;
     return (l-r);
+}
+
+int maxChildrenHeight (AVL_Node node) {
+  unsigned l, r;
+    l = (node.left)  ? node.left.height  : 0;
+    r = (node.right) ? node.right.height : 0;
+    return (l > r) ? l : r;
 }
--- AVL_tree.h
+++ AVL_tree.h
@@ -1,7 +1,7 @@
-/*== Stephanie Briere Americo
-		GRR20165313 ==*/
-/*== Talita Halboth Cunha Fernandes
-		GRR20165399 ==*/
+/* Stephanie Briere Americo
+	       GRR20165313         */
+/* Talita Halboth Cunha Fernandes
+	       GRR20165399         */
 
 #ifndef __AVL_Tree__
 #define __AVL_Tree__
@@ -11,14 +11,14 @@ typedef struct AVL_Node {
     struct AVL_Node *p;
     struct AVL_Node *left;
     struct AVL_Node *right;
-    // Height of the node, 
-    // the count starts on the sheet 
-    // and increases to the root.
-    // The height of the sheet is 0.
+    /* Height of the node, 
+       the count starts on the sheet 
+       and increases to the root.
+       The height of the sheet is 0. */
     unsigned int height; 
 } AVL_Node;
 
-void insertNode (AVL_Node *node, int key);
+AVL_Node* insertNode (AVL_Node *node, int key);
 void removeNode (AVL_Node *node, int key); 
 void searchNode (AVL_Node *node, int key);
 
