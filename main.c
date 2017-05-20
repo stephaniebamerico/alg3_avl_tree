a93803eb9aaf734b4291c245483be3dd586a210c
Stephanie Briere <sba16@inf.ufpr.br>
2017-05-20 20:56:01 -0300

    PRONTO?
    Signed-off-by: Stephanie Briere <sba16@inf.ufpr.br>

--- main.c
+++ main.c
@@ -3,6 +3,23 @@
 
 int main() {
     AVL_Node *root = NULL;
+    char input;
+    int key;
 
-
+    while (scanf("%c", input) > 0) {
+        switch (input) {
+            case 'i':
+                scanf("%d", key);
+                root = insertNode(key);
+                break;
+            case 'r':
+                scanf("%d", key);
+                root = removeNode(key);
+                break;
+            case 'b':
+                scanf("%d", key);
+                root = searchNode(key);
+                break;
+        }
+    }
 }
